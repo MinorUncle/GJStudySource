c7b404e72c7cc5d3c2d668e2a1877fc0222fd6f1
MinorUncle <1065517719@qq.com>
2016-09-29 19:00:50 +0800

    ruby

--- ruby/rubyTest.rb
+++ ruby/rubyTest.rb
@@ -1,10 +1,22 @@
 #!/usr/bin/ruby -w
 
-puts "Hello, Ruby!";
-print <<EOF
-    Hello
-    ruby
-    is this
-    you
-EOF
-puts print
+puts ARGV.each{|item|
+    puts "item #{item}"
+}
+#require 'xcodeproj'
+#project_path = '/Users/tongguan/Develop/Mp4v2Code/Mp4v2Code.xcodeproj'
+#project = Xcodeproj::Project.open(project_path)
+#target = project.targets[0]
+#group = project.main_group.find_subpath(File.join('util'), true)
+#file_ref = group.find_file_by_path('mp4art.cpp')
+#real_path = group.recursive_children()
+#addfile = target.add_file_references([file_ref])
+#puts "target:#{target}"
+#puts "group: #{group}"
+#puts "file_ref: #{file_ref}"
+#puts "addfile: #{addfile}"
+#project.save
+
+#project.targets.each do |target|
+#    puts target.name
+#end
